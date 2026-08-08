bd3d022d741e690cd2eca7fc7a6ca79c69afd714
Abhay Chauhan <ac804781@gmail.com>
2026-08-08 16:11:14 +0530

    add new items

---
+++ public/homeproductcard.js
@@ -0,0 +1,8 @@
+const productContainer = document.querySelector(".productContainer");
+const productTemplate = document.querySelector(".productTemplate");
+
+export const showProductContainer = (products) => {
+    if(!products){
+        return false;
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+import products from "./api/products.json";
+import { showProductContainer } from '../public/homeproductcard';
+
+//call the function to display all the items of productin a cards .
+
+showProductContainer(products);
