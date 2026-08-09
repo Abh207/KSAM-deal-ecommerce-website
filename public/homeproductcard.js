744dad68dfa4fb4dd974534cf05d6ddc9ecba95c
Abhay Chauhan <ac804781@gmail.com>
2026-08-10 00:04:14 +0530

    add new products

--- public/homeproductcard.js
+++ public/homeproductcard.js
@@ -1,8 +1,19 @@
-const productContainer = document.querySelector(".productContainer");
-const productTemplate = document.querySelector(".productTemplate");
+const productContainer = document.querySelector("#productContainer");
+const productTemplate = document.querySelector("#productTemplate");
 
 export const showProductContainer = (products) => {
     if(!products){
         return false;
     }
+    products.forEach((curProd) => {
+        const{brand,category,description,id,image,name,price,stock} = curProd;
+        const productClone = document.importNode(productTemplate.content,true);
+        productClone.querySelector(".productImage").src = image;
+        productClone.querySelector(".productImage").alt = name;
+        productClone.querySelector(".productName").textContent = name;
+
+        productContainer.append(productClone);
+
+
+    });
 };
