edaea8f7cc0d294a05e62ebd6ec6ed2af7272f79
Abhay Chauhan <ac804781@gmail.com>
2026-08-11 15:33:55 +0530

    update product images and styling

--- public/homeproductcard.js
+++ public/homeproductcard.js
@@ -1,3 +1,5 @@
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const productContainer = document.querySelector("#productContainer");
 const productTemplate = document.querySelector("#productTemplate");
 
@@ -6,14 +8,32 @@ export const showProductContainer = (products) => {
         return false;
     }
     products.forEach((curProd) => {
-        const{brand,category,description,id,image,name,price,stock} = curProd;
+        const{brand,category,description,id,image,name,price, Actualprice,stock} = curProd;
         const productClone = document.importNode(productTemplate.content,true);
+        productClone.querySelector("#cardValue").setAttribute("id",`card${id}`);
+
+
+
+
+
         productClone.querySelector(".productImage").src = image;
         productClone.querySelector(".productImage").alt = name;
         productClone.querySelector(".productName").textContent = name;
 
+        productClone.querySelector(".category").textContent = category;
+
+        productClone.querySelector(".productDescription").textContent = description;
+
+        productClone.querySelector(".productPrice").textContent = `$${price}`;
+
+        productClone.querySelector(".productActualPrice").textContent = `$${Actualprice}`;
+
+        productClone.querySelector(".productStack").textContent = stock;
+
+        
+        productClone.querySelector(".stockElement").addEventListener('click' , (event) =>{
+            homeQuantityToggle(event,id,stock);
+        });  
         productContainer.append(productClone);
-
-
     });
 };
