97aa7649f628e5dd6c92ff92e0e510e1e127390b
Abhay Chauhan <ac804781@gmail.com>
2026-08-12 19:03:50 +0530

    add new addToCart page

--- public/addToCart.js
+++ public/addToCart.js
@@ -1,6 +1,7 @@
 import { getCartProductFromLS } from "./getCartProducts";
 import { updateCartValue } from "./updateCartValue";
 
+getCartProductFromLS();
 export const addToCart = (event,id,stock) => {
     let arrLocalStorageProduct = getCartProductFromLS(); 
 
@@ -15,7 +16,26 @@ export const addToCart = (event,id,stock) => {
 
     price = price.replace("$","");
 
-    
+    let existingProd = arrLocalStorageProduct.find((curProd) => curProd.id === id);
+
+    console.log(existingProd);
+
+    if(existingProd && quantity > 1){
+        quantity = Number(existingProd.quantity) + Number(quantity);
+        price = Number(price * quantity);
+        let updatedCart = {id , quantity,price};
+
+        let updateCart=arrLocalStorageProduct.map((curProd) => {
+            return curProd.id === id ? updatedCart : curProd;
+
+        });
+        console.log(updateCart);
+        localStorage.setItem("cartProductLS",JSON.stringify(updateCart));
+    }
+
+    if (existingProd){
+        return false;
+    }
 
 
     price = Number(price * quantity);
