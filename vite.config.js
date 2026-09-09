aab9f8a8036b8f8f0733b1bcd96a2e7a462e5177
Abhay Chauhan <ac804781@gmail.com>
2026-09-09 19:47:11 +0530

    add new header style

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
                 // Dynamic header products
                 headerprod: resolve(
                     __dirname,
-                    "headerprod.html"
+                    "heardeprod.html"
                 )
             }
         }
